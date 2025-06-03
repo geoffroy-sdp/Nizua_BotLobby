dc593e4cb7d639d7302fd493d883def99165255c
Merzouz <geoffroymerlin@outlook.fr>
2025-06-04 01:10:55 +0200

    Add setup scripts for Python and Git installation, and create a virtual environment

--- setup.bat
+++ setup.bat
@@ -1,15 +1,58 @@
 @echo off
+SETLOCAL ENABLEEXTENSIONS
+color 0A
+
 echo.
 echo 
 ===================================
 Initializing the environment...
 echo ===================================
+echo.
 
-:: Vérifie si python est installé
+:: crée un dossier temporaire
+set "TEMP_DIR=%TEMP%\git_pyhton_install"
+mkdir "%TEMP_DIR%" >nul 2>&1
 
+:: verifie si python est installé
 where python >nul 2>nul
 if errorlevel 1 (
-    echo [ERREUR] Python n'est pas installé ou n'est pas dans le PATH.
+    echo [INFO] Python isnt downloaded. Downloading python3.12.1
+    powershell -Command "invoke-webrequest -uri 'https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe' -OutFile '%TEMP_DIR%\python_installer.exe'"
+    echo [INFO] Downloading python3.12.1 installer...
+    "%TEMP_DIR%\python_installer.exe" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+)
+
+:: verifie si git est installer
+where git >nul 2>nul
+if errorlevel 1 (
+    echo [INFO] Git isnt downloaded. Downloading git
+    powershell -Command "Invoke-WebRequest -Uri https://github.Com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-64-bit.exe -OutFile '%TEMP_DIR%\git_installer.exe'"
+    echo [INFO] Downloading git installer...
+    "%TEMP_DIR%\git_installer.exe" /VERYSILENT /NORESTART
+)
+
+:: clone le repo
+
+echo.
+echo [INFO] Cloning the repository...
+git clone https://github.com/geoffroy-sdp/Nizua_BotLobby
+
+:: verifie si il est cloner
+if not exist "Nizua_BotLobby" (
+    echo [ERREUR] Le clone du repository a echoue.
+    pause
+    exit /b 1
+)
+
+echo [INFO] Repository cloned successfully.
+
+:: se deplace dans le dossier du projet
+cd Nizua_BotLobby
+
+:: Vérifie si python est installé apres l'installation
+where python >nul 2>nul
+if errorlevel 1 (
+    echo [ERREUR] python isnt downloaded.
     pause
     exit /b 1
 )
